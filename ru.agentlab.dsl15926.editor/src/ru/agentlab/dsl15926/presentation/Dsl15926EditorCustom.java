5e98758438943dc87ecc1406106af1c9f60c3f44
Alexey Ivanov <amivanoff@gmail.com>
2012-09-25 02:02:34 +0400

    minor warning fixes

---
+++ ru.agentlab.dsl15926.editor/src/ru/agentlab/dsl15926/presentation/Dsl15926EditorCustom.java
@@ -0,0 +1,13 @@
+package ru.agentlab.dsl15926.presentation;
+
+import ru.agentlab.dsl15926.adapters.powerloom.PowerLoomAdapter;
+
+
+public class Dsl15926EditorCustom extends Dsl15926Editor{
+	
+	@Override
+	public void createModel() {
+		super.createModel();
+		editingDomain.getResourceSet().getResources().get(0).getContents().get(0).eAdapters().add(new PowerLoomAdapter());
+	}
+}
