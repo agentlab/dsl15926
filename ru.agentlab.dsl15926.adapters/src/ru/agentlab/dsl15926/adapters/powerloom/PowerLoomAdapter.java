354a5878c697fa5f42e2e21d22c60152ab0ae211
dimonina <shishkin_dimon@mail.ru>
2012-09-26 11:21:07 +0400

    PowerLoomAdapter in Work

--- ru.agentlab.dsl15926.adapters/src/ru/agentlab/dsl15926/adapters/powerloom/PowerLoomAdapter.java
+++ ru.agentlab.dsl15926.adapters/src/ru/agentlab/dsl15926/adapters/powerloom/PowerLoomAdapter.java
@@ -1,14 +1,11 @@
 package ru.agentlab.dsl15926.adapters.powerloom;
 
-import java.util.List;
-
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.util.EContentAdapter;
 
 import ru.agentlab.dsl15926.AbstractObject;
 import ru.agentlab.dsl15926.Dsl15926Package;
 import edu.isi.powerloom.PLI;
-import edu.isi.powerloom.PlIterator;
 
 public class PowerLoomAdapter extends EContentAdapter{
 
@@ -28,6 +25,7 @@ public class PowerLoomAdapter extends EContentAdapter{
 		super.notifyChanged(notification);
 		
 		if (notification.getNotifier() instanceof ru.agentlab.dsl15926.Class){
+			ru.agentlab.dsl15926.Class clazz = (ru.agentlab.dsl15926.Class) notification.getNotifier();
 			switch (notification.getFeatureID(ru.agentlab.dsl15926.Class.class)) {
 			case Dsl15926Package.CLASS__LABEL: {
 				switch (notification.getEventType()) {
@@ -48,14 +46,27 @@ public class PowerLoomAdapter extends EContentAdapter{
 				break;
 			}
 			case Dsl15926Package.CLASS__SUB_CLASS_OF: {
+				StringBuilder sb = new StringBuilder();
+				for (AbstractObject obj : clazz.getSubClassOf()) {
+					sb.append(" ");
+					sb.append(obj.getLabel());
+				}
+				String command = "(defconcept " + clazz.getLabel() + " (?x" + sb.toString() + "))";
+
+				if (command.length() > 0) {
+					PLI.sEvaluate(command, null, null);
+					System.out.println(command);
+				}
+				break;
+			}
+			case Dsl15926Package.CLASS__EQUIVALENT_CLASSES: {
 				switch (notification.getEventType()) {
-				case Notification.SET: {
-					break;
-				}
 				case Notification.ADD: {
-					break;
-				}
-				case Notification.ADD_MANY: {
+					String command = "(assert (synonym " + clazz.getLabel() + " " + ((ru.agentlab.dsl15926.Class)notification.getNewValue()).getLabel() + "))";
+					if (command.length() > 0) {
+						PLI.sEvaluate(command, null, null);
+						System.out.println(command);
+					}
 					break;
 				}
 				}
